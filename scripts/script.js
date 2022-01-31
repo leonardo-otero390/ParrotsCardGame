4a045a53c743dfbecc3ba86dcc866ff647da26ab
Leonardo Otero <leonardo.otero390@gmail.com>
2022-01-31 17:10:34 -0400

    fix: fast click

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 const deckTotal = [
-    "bobrossparrot",
-    "explodyparrot",
-    "fiestaparrot",
-    "metalparrot",
-    "revertitparrot",
-    "tripletsparrot",
-    "unicornparrot",
-]
+  "bobrossparrot",
+  "explodyparrot",
+  "fiestaparrot",
+  "metalparrot",
+  "revertitparrot",
+  "tripletsparrot",
+  "unicornparrot",
+];
 let cardsNumber;
 let cardsRotateds = 0;
 let cardsRAM = [];
@@ -14,85 +14,87 @@ let playsCounter = 0;
 let finalCondition = 0;
 let time = 0;
 function numberOfCards() {
-    cardsNumber = Number(prompt("Insira o número de cartas para o game"));
-    let itsPair = cardsNumber % 2;
-    while (cardsNumber < 4 || cardsNumber > 14 || itsPair != 0) {
-        cardsNumber = Number(prompt("Apenas números pares entre 4 e 14 são aceitos"));
-        itsPair = cardsNumber % 2;
-    }
+  cardsNumber = Number(prompt("Insira o número de cartas para o game"));
+  let itsPair = cardsNumber % 2;
+  while (cardsNumber < 4 || cardsNumber > 14 || itsPair != 0) {
+    cardsNumber = Number(
+      prompt("Apenas números pares entre 4 e 14 são aceitos")
+    );
+    itsPair = cardsNumber % 2;
+  }
 }
 function comparador() {
-    return Math.random() - 0.5;
+  return Math.random() - 0.5;
 }
 function shufleCards() {
-    deckTotal.sort(comparador);
-    const deckGame = [];
-    for (let i = 0; i < (cardsNumber / 2); i++) {
-        deckGame.push(deckTotal[i]);
-        deckGame.push(deckTotal[i]);
+  deckTotal.sort(comparador);
+  const deckGame = [];
+  for (let i = 0; i < cardsNumber / 2; i++) {
+    deckGame.push(deckTotal[i]);
+    deckGame.push(deckTotal[i]);
+  }
 
-    }
-
-    return deckGame.sort(comparador);
+  return deckGame.sort(comparador);
 }
 function cardDealer() {
-    const deckGame = shufleCards();
-    const element = document.querySelector(".game-table");
-    for (let i = 0; i < cardsNumber; i++) {
-        element.innerHTML += `<div class="card" onclick="rotateCard(this);">
+  const deckGame = shufleCards();
+  const element = document.querySelector(".game-table");
+  for (let i = 0; i < cardsNumber; i++) {
+    element.innerHTML += `<div class="card" onclick="rotateCard(this);">
         <div class="front-face face"><img src="images/front.png" alt="parrot"></div>
         <div class="back-face face"><img src="gifs/${deckGame[i]}.gif" alt="${deckGame[i]}"></div>
     </div>`;
-    }
+  }
 }
 function resetCards() {
-    for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < 2; j++) {
-            cardsRAM[i].card.querySelectorAll(".rotate")[0].classList.remove("rotate");
-        }
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 2; j++) {
+      cardsRAM[i].card
+        .querySelectorAll(".rotate")[0]
+        .classList.remove("rotate");
     }
+  }
 }
 function finalGame() {
-    if (finalCondition === cardsNumber) {
-        alert(`Você ganhou em ${playsCounter} jogadas! em ${time} segundos`)
-        const resetGame = prompt("Bora jogar de novo?");
-        if (resetGame === 'sim') {
-            document.querySelector(".game-table").innerHTML = "";
-            playsCounter = 0;
-            finalCondition = 0;
-            time = 0;
-            numberOfCards();
-            cardDealer();
-        }
+  if (finalCondition === cardsNumber) {
+    alert(`Você ganhou em ${playsCounter} jogadas! em ${time} segundos`);
+    const resetGame = prompt("Bora jogar de novo?");
+    if (resetGame === "sim") {
+      document.querySelector(".game-table").innerHTML = "";
+      playsCounter = 0;
+      finalCondition = 0;
+      time = 0;
+      numberOfCards();
+      cardDealer();
     }
+  }
 }
 function rotateCard(element) {
-    playsCounter++;
-    cardsRAM[cardsRotateds] = {
-        card: element,
-        name: element.querySelector(".back-face img").getAttribute("alt")
+  if (cardsRotateds === 2) return;
+  playsCounter++;
+  cardsRAM[cardsRotateds] = {
+    card: element,
+    name: element.querySelector(".back-face img").getAttribute("alt"),
+  };
+  element.querySelectorAll(".face")[0].classList.add("rotate");
+  element.querySelectorAll(".face")[1].classList.add("rotate");
+  cardsRotateds++;
+  if (cardsRotateds === 2) {
+    if (cardsRAM[0].name !== cardsRAM[1].name) {
+      setTimeout(resetCards, 1000);
+    } else {
+      finalCondition += 2;
+      setTimeout(finalGame, 300);
     }
-    element.querySelectorAll(".face")[0].classList.add("rotate");
-    element.querySelectorAll(".face")[1].classList.add("rotate");
-    cardsRotateds++;
-    if (cardsRotateds === 2) {
-        if (cardsRAM[0].name !== cardsRAM[1].name) {
-            setTimeout(resetCards, 1000);
-        } else {
-            finalCondition += 2;
-            setTimeout(finalGame, 300);
-
-        }
-        setTimeout(function () {
-            cardsRAM = [];
-            cardsRotateds = 0;
-        }, 1100)
-
-    }
+    setTimeout(function () {
+      cardsRAM = [];
+      cardsRotateds = 0;
+    }, 1100);
+  }
 }
 function timer() {
-    time++;
-    document.querySelector(".stop-clock").innerHTML = time;
+  time++;
+  document.querySelector(".stop-clock").innerHTML = time;
 }
 numberOfCards();
 cardDealer();
